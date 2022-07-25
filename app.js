506e8d582ecc6025c05acb4a534157aef45f2740
brainy2001 <khoshfekr2001@protonmail.com>
2022-07-26 01:23:48 +0430

    Now We can send signup.html with public folder that include css and js and bootstrap with images

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const axios = require('axios');
+/*const axios = require('axios');*/
 
 const app = express();
-app.use(bodyParser.urlencoded({extended : true}));
-app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
 
 app.get("/", function (req,res) {
    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/" , function (req,res) {
+   let firstName = req.body.firstName;
+   let lastName = req.body.lastName;
+   let email = req.body.email;
+
+   res.send("complete");
+   console.log(firstName , lastName, email);
 });
 
 app.listen(3000 , function () {
