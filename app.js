331a577b02213b8f29b6844fe9475b97caf13341
brainy2001 <khoshfekr2001@protonmail.com>
2022-07-26 20:25:13 +0430

    I download Mailchimp package and our server now can add Audience in Meilchimp

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-/*const axios = require('axios');*/
+const axios = require('axios');
+const https = require('https');
+const mailchimp = require('@mailchimp/mailchimp_marketing');
+
+
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
@@ -11,14 +15,45 @@ app.get("/", function (req,res) {
 });
 
 app.post("/" , function (req,res) {
-   let firstName = req.body.firstName;
-   let lastName = req.body.lastName;
-   let email = req.body.email;
+   const firstName = req.body.firstName;
+   const lastName = req.body.lastName;
+   const email = req.body.email;
 
-   res.send("complete");
-   console.log(firstName , lastName, email);
+   let data = {
+      members: [
+         {
+            email_address: email,
+            status: "subscribed",
+            merge_fields:{
+               FNAME: firstName,
+               LNAME: lastName
+            }
+         }
+      ]
+   };
+
+   let jsonData = JSON.stringify(data);
+
+   const url = "https://us14.api.mailchimp.com/3.0/lists/998d09491b"
+
+   const options = {
+      method: "POST",
+      auth:"brainy2001:132d9553cf7ba10820d8b6e948d46be7-us14"
+   }
+
+   const request = https.request(url, options, function (response) {
+      response.on("data", function (data) {
+         console.log(JSON.parse(data));
+      });
+   });
+
+   request.write(jsonData);
+   request.end();
 });
 
 app.listen(3000 , function () {
    console.log("Server is running on port 3000");
 });
+
+//APIkey : 132d9553cf7ba10820d8b6e948d46be7-us14
+//List Id : 998d09491b
